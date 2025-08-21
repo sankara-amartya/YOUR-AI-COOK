c6436e8182ce324afa5dfbf17b09350a93565241
sankara-amartya <amartyasankardas@gmail.com>
2025-08-21 21:11:34 +0530

    edits

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,5 +1,5 @@
 // React Router setup for page-based navigation
-import React from "react";
+//import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import App from "./app";
 
